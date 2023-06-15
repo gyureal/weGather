c0ddaed0197478921f6d84e23a8c4e41268a878c
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-16 07:09:56 +0900

    chore: init project

---
+++ src/main/java/com/example/wegather/WeGatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.wegather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeGatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wegather/WeGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wegather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
