7aa7e3bf8ab1f75192cd5b760174d9f8887f85d4
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-20 22:43:22 +0900

    feat: 예외처리 기능 추가
    RestControllerAdvice 를 통해 공통 에러 처리를 합니다.
    
    See also: Issue #2

---
+++ src/main/java/com/example/wegather/config/exceptionHandle/ExceptionAdvice.java
@@ -0,0 +1,46 @@
+package com.example.wegather.config.exceptionHandle;
+
+import java.util.UUID;
+import javax.security.sasl.AuthenticationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "알 수 없는 에러가 발생했습니다.";
+
+    /**
+     * 예외에 대한 로그 ID를 생성합니다.
+     * UUID 로 랜덤값을 생성한 후 로그를 남깁니다.
+     * @param ex
+     * @return
+     */
+    private UUID generateLogId(Exception ex) {
+        UUID uuid = UUID.randomUUID();
+        log.error("## error : {}, {}", uuid, ex.getClass().getSimpleName(), ex);
+        return uuid;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ErrorResponse.of(generateLogId(ex), ex);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleAuthenticationException(AuthenticationException ex) {
+        return ErrorResponse.of(generateLogId(ex), ex);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception ex) {
+        return ErrorResponse.of(generateLogId(ex), new Exception(INTERNAL_SERVER_ERROR_MESSAGE));
+    }
+}
