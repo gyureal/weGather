210f1ab28400eb3838f2be58db62c944029b7f75
gyureal <yonggyu.jeong10@gmail.com>
2023-06-21 23:18:46 +0900

    feat(interest): 관심사 조회, 삭제 기능 추가
    관심사 조회, 삭제 기능을 추가하였습니다.
    
    see Also: #4

--- src/main/java/com/example/wegather/interest/InterestService.java
+++ src/main/java/com/example/wegather/interest/InterestService.java
@@ -1,7 +1,7 @@
 package com.example.wegather.interest;
 
 import com.example.wegather.interest.dto.CreateInterestRequest;
-import java.util.Optional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class InterestService {
 
   private static final String INTEREST_NAME_ALREADY_EXISTS = "관심사 이름이 이미 존재합니다.";
+  private static final String INTEREST_NOT_FOUND = "해당 관심사를 찾을 수 없습니다.";
   private final InterestRepository interestRepository;
 
   /**
@@ -25,5 +26,32 @@ public class InterestService {
       throw new IllegalArgumentException(INTEREST_NAME_ALREADY_EXISTS);
     }
     return interestRepository.save(Interest.of(request.getInterestName()));
+  }
+
+  /**
+   * 전체 관심사를 조회합니다.
+   * @return
+   */
+  public List<Interest> getAllInterests() {
+    return interestRepository.findAll();
+  }
+
+  /**
+   * id에 해당하는 관심사를 조회합니다.
+   * @throws IllegalArgumentException id에 해당하는 관심사가 없는 경우 예외를 던집니다.
+   * @param id
+   * @return
+   */
+  public Interest getInterest(Long id) {
+    return interestRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException(INTEREST_NOT_FOUND));
+  }
+
+  /**
+   * id에 해당하는 관심사를 삭제합니다.
+   * @param id
+   */
+  public void deleteInterest(Long id) {
+    interestRepository.deleteById(id);
   }
 }
