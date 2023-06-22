ec2efd80513a676eabd45cf6f0437c501ef87b5c
gyureal <yonggyu.jeong10@gmail.com>
2023-06-22 21:34:17 +0900

    feat(member): PhoneNumber VO 생성
    see Also: #4

--- src/main/java/com/example/wegather/global/Message.java
+++ src/main/java/com/example/wegather/global/Message.java
@@ -6,5 +6,6 @@ public class Message {
     public static final String INTEREST_NOT_FOUND = "해당 관심사를 찾을 수 없습니다.";
     public static final String USERNAME_RULE_VIOLATION = "회원 아이디는 4-12자 사이의 영문 또는 숫자여야 합니다.";
     public static final String PASSWORD_RULE_VIOLATION = "비밀번호는 4-12자 사이의 영문 대소문자 또는 숫자여야합니다.";
+    public static final String PHONE_NUMBER_RULE_VIOLATION = "올바른 전화번호를 입력해주세요.";
   }
 }
--- src/main/java/com/example/wegather/global/vo/PhoneNumber.java
+++ src/main/java/com/example/wegather/global/vo/PhoneNumber.java
@@ -1,21 +1,27 @@
 package com.example.wegather.global.vo;
 
+import static com.example.wegather.global.Message.Error.PHONE_NUMBER_RULE_VIOLATION;
+
+import java.util.regex.Pattern;
 import lombok.Getter;
 
 @Getter
 public class PhoneNumber {
+  private static final Pattern PHONE_NUMBER_RULE = Pattern.compile("^01([0|1|6|7|8|9])-?(\\d{3,4})-?(\\d{4})$");
   private final String value;
 
   private PhoneNumber(String value) {
+    validatePhoneNumberRule(value);
     this.value = value;
   }
 
-  public PhoneNumber of(String value) {
-    validate(value);
+  public static PhoneNumber of(String value) {
     return new PhoneNumber(value);
   }
 
-  void validate(String value) {
-
+  void validatePhoneNumberRule(String value) {
+    if (!PHONE_NUMBER_RULE.matcher(value).matches()) {
+      throw new IllegalArgumentException(PHONE_NUMBER_RULE_VIOLATION);
+    }
   }
 }
