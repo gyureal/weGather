b215c42b8c61448825a089b0faad783019c9e2c3
gyureal <yonggyu.jeong10@gmail.com>
2023-06-22 22:12:36 +0900

    feat(member): Address VO 생성
    see Also: #4

--- src/main/java/com/example/wegather/global/vo/Address.java
+++ src/main/java/com/example/wegather/global/vo/Address.java
@@ -1,21 +1,30 @@
 package com.example.wegather.global.vo;
 
+import static com.example.wegather.global.Message.Error.STREET_ADDRESS_MUST_NOT_EMPTY;
+
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 @Getter
 public class Address {
-  private final String value;
+  private final String streetAddress; // 도로명주소
+  private final Double longitude;   // 경도
+  private final Double latitude;   // 위도
 
-  private Address(String value) {
-    this.value = value;
+  public Address(String streetAddress, Double longitude, Double latitude) {
+    validateAddress(streetAddress, longitude, latitude);
+    this.streetAddress = streetAddress;
+    this.longitude = longitude;
+    this.latitude = latitude;
   }
 
-  public Address of(String value) {
-    validate(value);
-    return new Address(value);
+  public static Address of(String streetAddress, Double longitude, Double latitude) {
+    return new Address(streetAddress, longitude, latitude);
   }
 
-  void validate(String value) {
-
+  void validateAddress(String streetAddress, Double longitude, Double latitude) {
+    if (!StringUtils.hasText(streetAddress)) {
+      throw new IllegalArgumentException(STREET_ADDRESS_MUST_NOT_EMPTY);
+    }
   }
 }
