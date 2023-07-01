fb2a2ad9e0e849b2633d4f60bb6193e969382251
gyureal <yonggyu.jeong10@gmail.com>
2023-07-01 12:16:09 +0900

    feat(global): getPrincipal 추가

--- src/main/java/com/example/wegather/global/auth/AuthenticationManager.java
+++ src/main/java/com/example/wegather/global/auth/AuthenticationManager.java
@@ -1,5 +1,6 @@
 package com.example.wegather.global.auth;
 
+import com.example.wegather.auth.MemberDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -7,8 +8,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthenticationManager implements IAuthenticationManager {
 
+  private static final String CANNOT_GET_PRINCIPAL_AS_MEMBER_DETAILS_TYPE = "MemberDetails 타입으로 Principal을 꺼낼 수 없습니다.";
+
   @Override
   public Authentication getAuthentication() {
     return SecurityContextHolder.getContext().getAuthentication();
+  }
+
+  @Override
+  public MemberDetails getPrincipal() {
+    Object principal = getAuthentication().getPrincipal();
+    if (principal instanceof MemberDetails) {
+      return (MemberDetails) principal;
+    }
+    throw new IllegalStateException(CANNOT_GET_PRINCIPAL_AS_MEMBER_DETAILS_TYPE);
   }
 }
--- src/main/java/com/example/wegather/global/auth/IAuthenticationManager.java
+++ src/main/java/com/example/wegather/global/auth/IAuthenticationManager.java
@@ -1,7 +1,10 @@
 package com.example.wegather.global.auth;
 
+import com.example.wegather.auth.MemberDetails;
 import org.springframework.security.core.Authentication;
 
 public interface IAuthenticationManager {
   Authentication getAuthentication();
+
+  MemberDetails getPrincipal();
 }
