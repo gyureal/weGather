8e472b9e21c950efad7ed90c9ede1577203d9e43
gyureal <yonggyu.jeong10@gmail.com>
2023-08-21 22:55:48 +0900

    Revert "feat(smallGroupJoin): DB, Redis 접속정보 암호화, 개발 DB RDS 주소로 변경"
    This reverts commit c910eccafa94782855607c2368a2e6f5259d7dda.

--- src/test/java/com/example/wegather/config/TestConfig.java
+++ src/test/java/com/example/wegather/config/TestConfig.java
@@ -3,10 +3,6 @@ package com.example.wegather.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -20,5 +16,4 @@ public class TestConfig {
   public JPAQueryFactory jpaQueryFactory() {
     return new JPAQueryFactory(entityManager);
   }
-
 }
