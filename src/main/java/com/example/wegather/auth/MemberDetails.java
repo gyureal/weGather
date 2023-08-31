92e5f6f748341aa48345cd5a254cfe97c1946d69
gyureal <yonggyu.jeong10@gmail.com>
2023-08-31 20:04:52 +0900

    feat: CustomUserDetails에 회원 id 정보 포함
    - 회원 ID가 key 값이기 때문에 SpringSecurityContext에 회원의 id를 가지고 있게 하기 위해 추가하였습니다.
    - #21

--- src/main/java/com/example/wegather/auth/MemberDetails.java
+++ src/main/java/com/example/wegather/auth/MemberDetails.java
@@ -1,6 +1,7 @@
 package com.example.wegather.auth;
 
 import com.example.wegather.global.vo.MemberType;
+import com.example.wegather.member.domain.entity.Member;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -13,9 +14,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 @RequiredArgsConstructor
 @Builder
 public class MemberDetails implements UserDetails {
+  private final Long id;
   private final String username;
   private final String password;
   private final MemberType role;
+
+  public static MemberDetails from(Member member) {
+    return MemberDetails.builder()
+        .id(member.getId())
+        .username(member.getUsername().getValue())
+        .password(member.getPassword().getValue())
+        .role(member.getMemberType())
+        .build();
+  }
 
   public Boolean isAdmin() {
     return role == MemberType.ROLE_ADMIN;
