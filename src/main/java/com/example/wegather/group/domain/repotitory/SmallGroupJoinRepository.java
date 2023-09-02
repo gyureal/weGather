2e4e66fbc34aeff6b0ea874723470c75a278dec0
gyureal <yonggyu.jeong10@gmail.com>
2023-09-02 12:18:32 +0900

    feat: 소모임 가입 repository 생성
    - #26

---
+++ src/main/java/com/example/wegather/group/domain/repotitory/SmallGroupJoinRepository.java
@@ -0,0 +1,8 @@
+package com.example.wegather.group.domain.repotitory;
+
+import com.example.wegather.group.domain.entity.SmallGroupJoin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SmallGroupJoinRepository extends JpaRepository<SmallGroupJoin, Long> {
+
+}
