91dfb0f2b9dc32a58bc6bb061b7e973039bf474d
gyureal <yonggyu.jeong10@gmail.com>
2023-10-09 10:34:54 +0900

    feat: cors 허용을 위한 조치 추가

--- src/main/java/com/example/wegather/config/WebSecurityConfig.java
+++ src/main/java/com/example/wegather/config/WebSecurityConfig.java
@@ -1,15 +1,18 @@
 package com.example.wegather.config;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Slf4j
 @Configuration
@@ -23,11 +26,23 @@ public class WebSecurityConfig {
     http.authorizeRequests()
         .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
         .antMatchers("/", "/view", "/view/sign-in", "/view/sign-up", "/auth/me", "/check-email-token").permitAll()
-        .antMatchers(HttpMethod.POST,"/sign-up", "/sign-in", "/check-email-token").permitAll()
+        .antMatchers("/api/sign-up", "/api/sign-in", "/api/check-email-token").permitAll()
         .antMatchers("/health").permitAll()
         .anyRequest().authenticated();
-
+    http.cors().configurationSource(corsConfigurationSource());
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedHeader("*");
+    //configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
