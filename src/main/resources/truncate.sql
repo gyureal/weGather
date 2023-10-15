819741e35da6ea3cd5e24bdd46fdb9175c90b545
gyureal <yonggyu.jeong10@gmail.com>
2023-10-15 22:05:27 +0900

    fix: truncate.sql 에 member_alarm_setting 테이블 추가
    - 테스트 고립을 위해 테이블의 데이터 삭제

--- src/main/resources/truncate.sql
+++ src/main/resources/truncate.sql
@@ -3,9 +3,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE interest;
 TRUNCATE TABLE member;
 TRUNCATE TABLE member_interest;
+TRUNCATE TABLE member_alarm_setting;
 TRUNCATE TABLE small_group;
 TRUNCATE TABLE small_group_interest;
 TRUNCATE TABLE small_group_join;
 TRUNCATE TABLE small_group_member;
+
 
 
