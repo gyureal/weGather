79c01231a9b98bf5df7328d7a6ec51401ee0fcf5
gyureal <yonggyu.jeong10@gmail.com>
2023-11-03 19:22:26 +0900

    refactor: 소모임 회원 DTO 컬럼명 변경 (isManager)

--- src/main/java/com/example/wegather/group/dto/ManagerAndMemberDto.java
+++ src/main/java/com/example/wegather/group/dto/ManagerAndMemberDto.java
@@ -1,6 +1,7 @@
 package com.example.wegather.group.dto;
 
 import com.example.wegather.groupJoin.domain.entity.SmallGroupMember;
+import com.example.wegather.groupJoin.domain.vo.SmallGroupMemberType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,14 +12,14 @@ public class ManagerAndMemberDto {
   private String name;
   private String introduction;
   private String image;
-  private String memberType;
+  private boolean isManager;
 
   public static ManagerAndMemberDto from(SmallGroupMember groupMember) {
     return ManagerAndMemberDto.builder()
         .name(groupMember.getMember().getUsername())
         .introduction(groupMember.getMember().getIntroductionText())
         .image(groupMember.getMember().getProfileImage())
-        .memberType(groupMember.getSmallGroupMemberType().name())
+        .isManager(groupMember.getSmallGroupMemberType() == SmallGroupMemberType.MANAGER)
         .build();
   }
 }
