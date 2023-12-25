0f28344d0b6461b6bcfb0f429c6f64233e93a8c3
gyureal <yonggyu.jeong10@gmail.com>
2023-12-25 21:33:55 +0900

    chore: 테스트 스크립트 수정
    - -it 옵션제거
    - #55

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock -e TESTCONTAINERS_HOST_OVERRIDE=host.docker.internal gradle:latest gradle test
+docker run --rm -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock -e TESTCONTAINERS_HOST_OVERRIDE=host.docker.internal gradle:latest gradle test
