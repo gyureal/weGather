d8045afcacd2aa70f8ca4e3e1024369de08f7678
gyureal <yonggyu.jeong10@gmail.com>
2023-12-27 12:36:21 +0900

    chore: 설정 경로 환경 변수 설정 추가

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -5,5 +5,8 @@ echo 'stop running container'
 docker stop wegather
 docker rm wegather
 
+echo 'set setting file directory'
+export ENV_DIR=/home/ec2-user/app/wegather/env
+
 echo 'run deployed container'
-docker run -d --name wegather -p 8080:8080 $REGISTRY/$IMAGE_NAME:$TAG
+docker run -d --name wegather -v $ENV_DIR:/app/env -e ENV_DIR=/app/env -p 8080:8080 $REGISTRY/$IMAGE_NAME:$TAG
