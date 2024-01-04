ddb0dc92c08c217fc467cd19022264e59d434e46
prebird <yonggyu.jeong10@gmail.com>
2024-01-04 18:50:23 +0900

    refactor: current-user 로그 추가

--- src/main/java/com/example/wegather/auth/AuthController.java
+++ src/main/java/com/example/wegather/auth/AuthController.java
@@ -11,6 +11,7 @@ import com.example.wegather.member.dto.MemberDto;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
@@ -75,6 +77,7 @@ public class AuthController {
    */
   @GetMapping("/current-user")
   public ResponseEntity<MemberInfo> getMyInfo(@AuthenticationPrincipal MemberDetails memberDetails) {
+    log.info("current-user called");
     if (memberDetails != null) {
       Member member = memberRepository.findWithAlarmSettingByUsername(memberDetails.getUsername())
           .orElseThrow(() -> new AuthenticationException(ErrorCode.MEMBER_NOT_FOUND.getDescription()));
