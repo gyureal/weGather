1b99da23c91327cd09298c0164d39a9ae4711291
prebird <78974381+prebird@users.noreply.github.com>
2024-04-03 15:11:15 +0900

    [#59] refactor: UploadFile 객체 내 변수명 변경

--- src/main/java/com/example/wegather/global/upload/UploadFile.java
+++ src/main/java/com/example/wegather/global/upload/UploadFile.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class UploadFile {
-  private String uploadFileName;  // 기존의 파일 이름
-  private String storeFileName;   // 업로드된 파일의 이름
+  private String originalFileName;  // 기존의 파일 이름
+  private String storedFileName;   // 업로드된 파일의 이름
 
-  public UploadFile(String uploadFileName, String storeFileName) {
-    this.uploadFileName = uploadFileName;
-    this.storeFileName = storeFileName;
+  public UploadFile(String originalFileName, String storedFileName) {
+    this.originalFileName = originalFileName;
+    this.storedFileName = storedFileName;
   }
 
   public static UploadFile of(String uploadFileName, String storeFileName) {
