884f19ad2524637f195753065f10c29bcf0888f2
prebird <78974381+prebird@users.noreply.github.com>
2024-04-03 20:20:19 +0900

    [#59] feat: 업로드 하는 이미지의 경로를 날짜로 구분하도록 변경

--- src/main/java/com/example/wegather/global/upload/repository/AbstractFileManager.java
+++ src/main/java/com/example/wegather/global/upload/repository/AbstractFileManager.java
@@ -1,9 +1,42 @@
 package com.example.wegather.global.upload.repository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 import org.apache.commons.codec.binary.Base64;
 
 public abstract class AbstractFileManager implements FileManager {
+  private static final DateTimeFormatter simpleDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+  /**
+   * 저장할 파일 이름을 생성합니다.
+   * 1. 생성한 날짜로 폴더를 구분합니다.
+   * 2. Unique 한 이름을 생성하여 반환합니다.
+   * 3. UUID 에 기존 파일의 확장자를 붙혀서 만듭니다.
+   * @param originalFilename 기존 이미지명
+   * @return 저장할 파일명 (ex) 2024/2024-01-29/UUID.jpeg)
+   */
+  public String createStoreFileName(String originalFilename) {
+    // 날짜
+    LocalDateTime now = LocalDateTime.now();
+    int year = now.getYear();
+    String todayDate = now.format(simpleDateFormatter);
+    // 확장자
+    String ext = extractExt(originalFilename);
+    String uuid = UUID.randomUUID().toString();
+    // 조합
+    return "images/" + year + "/" + todayDate + "/" + uuid + "." + ext;
+  }
+
+  /**
+   * 파일의 확장자를 반환합니다.
+   * @param originalFilename
+   * @return
+   */
+  String extractExt(String originalFilename) {
+    int pos = originalFilename.lastIndexOf(".");
+    return originalFilename.substring(pos + 1);
+  }
 
   /**
    * Base64 파일을 decode 하여 반환합니다.
@@ -27,28 +60,5 @@ public abstract class AbstractFileManager implements FileManager {
   @Deprecated
   private String parseOnlyImageFromBase64(String base64File) {
     return base64File.split(",")[1];
-  }
-
-  /**
-   * 저장할 파일 이름을 생성합니다.
-   * Unique 한 이름을 생성하여 반환합니다.
-   * UUID 에 기존 파일의 확장자를 붙혀서 만듭니다.
-   * @param originalFilename
-   * @return
-   */
-  String createStoreFileName(String originalFilename) {
-    String ext = extractExt(originalFilename);
-    String uuid = UUID.randomUUID().toString();
-    return uuid + "." + ext;
-  }
-
-  /**
-   * 파일의 확장자를 반환합니다.
-   * @param originalFilename
-   * @return
-   */
-  String extractExt(String originalFilename) {
-    int pos = originalFilename.lastIndexOf(".");
-    return originalFilename.substring(pos + 1);
   }
 }
