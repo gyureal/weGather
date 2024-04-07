8205695ae36c766cc4c5b6886dbd7a28984f3b6c
prebird <78974381+prebird@users.noreply.github.com>
2024-04-07 19:57:36 +0900

    [#66] feat: 관심사 수정시, cache invalidation 로직 추가

--- src/main/java/com/example/wegather/interest/domain/InterestService.java
+++ src/main/java/com/example/wegather/interest/domain/InterestService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class InterestService {
-
+  private static final String GET_WHITELIST_CACHE_NAME = "getInterestWhiteList";
   private final InterestRepository interestRepository;
 
   /**
@@ -25,6 +26,7 @@ public class InterestService {
    * @return 생성된 관심사
    * @throws IllegalArgumentException 관심사 이름이 이미 존재하는 경우 예외를 던집니다.
    */
+  @CacheEvict(value = GET_WHITELIST_CACHE_NAME, allEntries = true)
   public Interest addInterest(CreateInterestRequest request) {
 
     boolean isNameExists = interestRepository.existsByName(request.getInterestName());
@@ -56,8 +58,9 @@ public class InterestService {
    * 관심사 화이트리스트를 조회합니다.
    * @return
    */
-  @Cacheable(value = "getInterestWhiteList")
+  @Cacheable(value = GET_WHITELIST_CACHE_NAME)
   public List<String> getInterestWhiteList() {
+    log.info("## uncached ##");
     return interestRepository.findAll().stream().map(Interest::getName).collect(Collectors.toList());
   }
 
@@ -76,6 +79,7 @@ public class InterestService {
    * id에 해당하는 관심사를 삭제합니다.
    * @param id
    */
+  @CacheEvict(value = GET_WHITELIST_CACHE_NAME, allEntries = true)
   public void deleteInterest(Long id) {
     interestRepository.deleteById(id);
   }
